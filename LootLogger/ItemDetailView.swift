93ac708234a3dd7b1a54985c4c40acb4e71f5b86
mindgraffiti <thuycopeland@gmail.com>
2019-08-25 15:04:00 -0600

    Add a new file, ItemDetailView

---
+++ LootLogger/ItemDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  ItemDetailView.swift
+//  LootLogger
+//
+//  Created by Thuy Copeland on 8/25/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetailView()
+    }
+}
