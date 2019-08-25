a30ac8cc4548321a5592384164ff0bdc2a6383c0
mindgraffiti <thuycopeland@gmail.com>
2019-08-25 15:09:29 -0600

    Start building out the detail view

--- LootLogger/ItemDetailView.swift
+++ LootLogger/ItemDetailView.swift
@@ -8,9 +8,27 @@
 
 import SwiftUI
 
+struct FormField: View {
+    let title: String
+    let placeholder: String
+    @Binding var value: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+            TextField("plaecholder",
+                      text: $value) // dollar sign means the bindable value
+        }
+    }
+}
+
 struct ItemDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            FormField(title: "Name:",
+                      placeholder: "",
+                      value: .constant("Item Name"))
+        }
     }
 }
 
